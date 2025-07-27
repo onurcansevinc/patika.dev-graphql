a1ba482a19249ed805ec2033c6dd3dfb5f78ba38
Onurcan Sevinc <onurcan.svnc@yandex.com>
2025-07-28 00:19:28 +0300

    feat: enhance GraphQL schema and resolvers to support user, event, location, and participant queries

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const typeDefs = gql`
         to: String
         location_id: Int
         user_id: Int
+        user: User
+        participants: Participant
+        location: Location
     }
 
     type Location {
@@ -32,22 +35,62 @@ const typeDefs = gql`
         id: Int
         user_id: Int
         event_id: Int
+        user: User
     }
 
     type Query {
         users: [User!]
+        user(id: Int!): User
+
         events: [Event!]
+        event(id: Int!): Event
+
         locations: [Location!]
+        location(id: Int!): Location
+
         participants: [Participant!]
+        participant(id: Int!): Participant
     }
 `;
 
 const resolvers = {
     Query: {
         users: () => data.users,
+        user: (parent, args) => {
+            return data.users.find((user) => user.id == args.id);
+        },
+
         events: () => data.events,
+        event: (parent, args) => {
+            return data.events.find((event) => event.id == args.id);
+        },
+
         locations: () => data.locations,
+        location: (parent, args) => {
+            return data.locations.find((location) => location.id == args.id);
+        },
+
         participants: () => data.participants,
+        participant: (parent, args) => {
+            return data.participants.find((participant) => participant.id == args.id);
+        },
+    },
+
+    Event: {
+        user: (parent, args) => {
+            return data.users.find((user) => user.id == parent.user_id);
+        },
+        participants: (parent, args) => {
+            return data.participants.find((participant) => participant.event_id == parent.id);
+        },
+        location: (parent, args) => {
+            return data.locations.find((location) => location.id == parent.location_id);
+        },
+    },
+    Participant: {
+        user: (parent, args) => {
+            return data.users.find((user) => user.id == parent.user_id);
+        },
     },
 };
 
